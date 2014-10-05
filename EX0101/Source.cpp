288b56a006b59951dfa0a96cc2470466e3c6d7fb
Kevin Wung <kevinwung@foxmail.com>
2014-10-05 22:05:05 +0800

    The 1st time commit

---
+++ EX0101/Source.cpp
@@ -0,0 +1,11 @@
+//Êä³ö¡°I am a student."£¬²¢»»ÐÐ¡£
+//#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+//	cout << "I am a student." << "\n";
+	printf("I am a student.\n");
+	getchar();
+	return 0;
+}
